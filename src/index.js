6746cf72eefbc7d5ede01b7d227d0018cacae5fb
uxuisooa <sooa0513@naver.com>
2022-10-19 13:25:02 +0900

    auth 리덕스 모듈에서 api 사용하기

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./modules";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from '@reduxjs/toolkit';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
 
-const store = configureStore({ reducer: rootReducer });
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,9 +1,19 @@
-import produce from "immer";
-import { createAction, handleActions } from "redux-actions";
+import produce from 'immer';
+import { createAction, handleActions } from 'redux-actions';
+import { takeLatest } from 'redux-saga/effects';
+import createRequestSaga, {
+  createRequestActionType,
+} from '../lib/createRequestSaga';
+import * as authAPI from '../lib/api/auth';
 // import produce from "immer";
 
-const CHANGE_FIELD = "auth/CHANGE_FIELD";
-const INITIALIZE_FORM = "auth/INITIALIZE_FORM";
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] =
+  createRequestActionType('auth/REGISTER');
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] =
+  createRequestActionType('auth/LOGIN');
 
 export const changeField = createAction(
   CHANGE_FIELD,
@@ -18,16 +28,36 @@ export const changeField = createAction(
 // register / login
 export const initalizeFrom = createAction(INITIALIZE_FORM, (form) => form);
 
+export const register = createAction(REGISTER, ({ username, password }) => ({
+  username,
+  password,
+}));
+
+export const login = createAction(LOGIN, ({ username, password }) => ({
+  username,
+  password,
+}));
+
+// 사가 생성
+const registerSaga = createRequestSaga(REGISTER, authAPI.register);
+const loginSaga = createRequestSaga(LOGIN, authAPI.register);
+export function* authSaga() {
+  yield takeLatest(REGISTER, registerSaga);
+  yield takeLatest(LOGIN, loginSaga);
+}
+
 const initialState = {
   register: {
-    username: "",
-    password: "",
-    passwordConfirm: "",
+    username: '',
+    password: '',
+    passwordConfirm: '',
   },
   login: {
-    username: "",
-    password: "",
+    username: '',
+    password: '',
   },
+  auth: null,
+  authError: null,
 };
 
 const auth = handleActions(
@@ -39,6 +69,29 @@ const auth = handleActions(
     [INITIALIZE_FORM]: (state, { payload: { form } }) => ({
       ...state,
       [form]: initialState[form],
+      authError: null, // 폼 전환 시 회원 인증 에러 초기화
+    }),
+    // 회원가입 성공
+    [REGISTER_SUCCESS]: (state, { payload: auth }) => ({
+      ...state,
+      authError: null,
+      auth,
+    }),
+    // 회원가입 실패
+    [REGISTER_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      authError: null,
+    }),
+    // 로그인 성공
+    [LOGIN_SUCCESS]: (state, { payload: auth }) => ({
+      ...state,
+      authError: null,
+      auth,
+    }),
+    // 로그인 실패
+    [LOGIN_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      authError: null,
     }),
   },
   initialState
@@ -46,8 +99,8 @@ const auth = handleActions(
 
 export default auth;
 
-/*  
-리덕스 모듈 : 
-Ducks 패턴을 사용하여 액션 타입, 액션 생성 함수, 리듀서가 
-하나의 파일에 다 정의되어 있는 리덕스 모듈을 작성.
-*/
+/*
+ * 리덕스 모듈 :
+ * Ducks 패턴을 사용하여 액션 타입, 액션 생성 함수, 리듀서가
+ * 하나의 파일에 다 정의되어 있는 리덕스 모듈을 작성.
+ */
